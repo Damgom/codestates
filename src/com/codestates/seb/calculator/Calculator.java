d4611f5ec8f53c3b08102430966fcdc5815de0e5
Damgom <wisewish1@gmail.com>
2022-05-09 14:50:53 +0900

    first version

--- src/com/codestates/seb/calculator/Calculator.java
+++ src/com/codestates/seb/calculator/Calculator.java
@@ -14,5 +14,46 @@ public class Calculator {
             4. 연산 결과를 콘솔에 출력합니다.
         */
     // TODO...
+    char operator;
+    double num1, num2, result;
+
+    System.out.println("첫번째 숫자를 입력");
+    num1 = input.nextDouble();
+
+    System.out.println("기호를 입력");
+    operator = input.next().charAt(0);
+
+    System.out.println("두번째 숫자를 입력");
+    num2 = input.nextDouble();
+
+    //스위치문 시작
+    switch (operator) {
+      case '+':
+        result = num1 + num2;
+        //더하기 출력
+        System.out.println(num1 + " + " + num2 + " = " + result + "입니다");
+        break;
+
+      case '-':
+        result = num1 - num2;
+        //빼기 출력
+        System.out.println(num1 + " - " + num2 + " = " + result + "입니다");
+        break;
+
+      case '*':
+        result = num1 * num2;
+        //곱하기 출력
+        System.out.println(num1 + " * " + num2 + " = " + result + "입니다");
+        break;
+
+      case '/':
+        result = num1 / num2;
+        //나누기 출력
+        System.out.println(num1 + " / " + num2 + " = " + result + "입니다");
+        break;
+
+      default:
+        System.out.println("---Syntax Error---");
+    } input.close();
   }
 }
