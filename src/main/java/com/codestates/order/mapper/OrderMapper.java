c4c420a3ba544fd85e05cc220cd5ba945abb37f9
Damgom <wisewish1@gmail.com>
2022-07-06 17:44:38 +0900

    -ing

--- src/main/java/com/codestates/order/mapper/OrderMapper.java
+++ src/main/java/com/codestates/order/mapper/OrderMapper.java
@@ -1,18 +1,36 @@
 package com.codestates.order.mapper;
 
 import com.codestates.coffee.entity.Coffee;
+import com.codestates.order.dto.OrderCoffeeResponseDto;
 import com.codestates.order.dto.OrderPatchDto;
 import com.codestates.order.dto.OrderPostDto;
 import com.codestates.order.dto.OrderResponseDto;
 import com.codestates.order.entity.Order;
+import com.codestates.order.entity.OrderCoffee;
 import org.mapstruct.Mapper;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface OrderMapper {
-    Order orderPostDtoToOrder(OrderPostDto orderPostDto);
+//    Order orderPostDtoToOrder(OrderPostDto orderPostDto);
     Order orderPatchDtoToOrder(OrderPatchDto orderPatchDto);
-    OrderResponseDto orderToOrderResponseDto(Order order, List<Coffee> coffees);
+//    OrderResponseDto orderToOrderResponseDto(Order order, List<Coffee> coffees);
     List<OrderResponseDto> ordersToOrderResponseDtos(List<Order> orders);
+
+    default OrderResponseDto orderToOrderResponseDto(Order order){
+        List<OrderCoffee> orderCoffees = order.getOrderCoffees();
+
+        OrderResponseDto orderResponseDto = new OrderResponseDto();
+        orderResponseDto.setOrderId(order.getOrderId());
+        orderResponseDto.setMember(order.getMember());
+        orderResponseDto.setOrderStatus(order.getOrderStatus());
+        orderResponseDto.setCreatedAt(order.getCreatedAt());
+        orderResponseDto.setOrderCoffees(orderCoffeesToOrderCoffeeResponseDto(orderCoffees));
+        return orderResponseDto;
+    }
+
+    default List<OrderCoffeeResponseDto> orderCoffeesToOrderCoffeeResponseDto(List<OrderCoffee> orderCoffees){
+        return orderCoffees
+    }
 }
